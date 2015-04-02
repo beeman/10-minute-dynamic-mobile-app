7fb02c4bde52f18aca69e5fb19a9f833e481931c
Bram Borggreve <borggreve@gmail.com>
2015-04-02 06:53:59 +0200

    Refactor controllers to use SDK

--- mobile-app/www/js/controllers.js
+++ mobile-app/www/js/controllers.js
@@ -4,56 +4,20 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('CategoriesCtrl', function($scope) {
-
-  $scope.categories = [
-    { id: 1, name: 'Cat 1'},
-    { id: 2, name: 'Cat 2'},
-    { id: 3, name: 'Cat 3'},
-    { id: 4, name: 'Cat 4'},
-  ];
-
+.controller('CategoriesCtrl', function($scope, Category) {
+  $scope.categories = Category.find();
 })
 
-.controller('CategoryCtrl', function($scope, $stateParams){
-
-  $scope.category = {
-    id: $stateParams.categoryId,
-    name: 'Cat ' + $stateParams.categoryId
-  }
-
-  $scope.products = [
-    { id: 1, name: 'Product 1'},
-    { id: 2, name: 'Product 2'},
-    { id: 3, name: 'Product 3'},
-    { id: 4, name: 'Product 4'},
-  ];
-
+.controller('CategoryCtrl', function($scope, $stateParams, Category, Product){
+  $scope.category = Category.findOne($stateParams.categoryId);
+  $scope.products = Product.find({
+    filter: { where: { categoryId: $stateParams.categoryId } }
+  });
 })
 
-.controller('ProductCtrl', function($scope, $stateParams){
-
-  $scope.category = {
-    id: $stateParams.categoryId,
-    name: 'Cat ' + $stateParams.categoryId
-  }
-
-  $scope.product = {
-    id: $stateParams.productId,
-    name: 'Product ' + $stateParams.productId
-  }
+.controller('ProductCtrl', function($scope, $stateParams, Category, Product){
+  $scope.category = Category.findOne($stateParams.categoryId);
+  $scope.product = Product.findOne($stateParams.productId);
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+;
